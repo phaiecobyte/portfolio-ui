c39bf67dd8b4d98b5361681338e3a1313b18139a
phaiecobyte <brave6823@gmail.com>
2025-08-09 01:06:51 +0700

    fix route problem

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { ProfileService } from '../../services/profile.service';
 
@@ -17,10 +17,17 @@ export class Home implements OnInit{
     "bio":"I'm a recent graduate from Build Bright University and currently interning as a Full-Stack Web Developer using ASP.NET MVC. I'm passionate about building innovative and practical solutions, with a strong interest in contributing to open-source projects. 🔭 Currently focused on backend development using .NET technologies 🌱 Learning Spring Boot and Microservice Architecture 💡 Constantly exploring new technologies, clean code practices, and scalable system design 🤝 Enjoy sharing knowledge and collaborating with the developer community"
   }
   constructor(
-    private myProfileService:ProfileService,
+    private router:Router
   ){}
   ngOnInit(): void {
     
   }   
+  
+  goToProject(){
+    this.router.navigate(['/projects'])
+  }
+  gotToContact(){
+    this.router.navigate(['/contact']);
+  }
   
 }
